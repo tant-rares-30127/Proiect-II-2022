4ee4664a8961286974ee0898aa99450ceebd66a5
Tant Rares <vnv_dom@yahoo.com>
2022-03-02 20:26:17 +0200

    First commit

---
+++ Proiect_II/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Proiect_II.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
