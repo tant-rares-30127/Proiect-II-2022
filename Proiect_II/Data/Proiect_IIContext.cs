5b1fba939a2e1362aca85701c707b04baaf56663
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-03-12 10:26:25 +0200

    User, Address models and UserController added.

---
+++ Proiect_II/Data/Proiect_IIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Proiect_II.Models;
+
+namespace Proiect_II.Data
+{
+    public class Proiect_IIContext : DbContext
+    {
+        public Proiect_IIContext (DbContextOptions<Proiect_IIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Proiect_II.Models.User> User { get; set; }
+    }
+}
---
+++ Proiect_II/Models/Address.cs
@@ -0,0 +1,10 @@
+﻿namespace Proiect_II.Models
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string Details { get; set; }
+    }
+}
