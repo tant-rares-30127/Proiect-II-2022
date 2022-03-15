c643830f51a4215864f6324e8f42cb32c900b697
DESKTOP-0KQ1UDC\tantr <tantrares@yahoo.com>
2022-03-15 19:49:09 +0200

    Adding new migration for 1on1 relationship

--- Proiect_II/Models/ShoppingCart.cs
+++ Proiect_II/Models/ShoppingCart.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace Proiect_II.Models
     {
         public int Id { get; set; }
         public DateTime DateTime { get; set; }
+        public int UserId { get; set; }
         public virtual User User { get; set; }
     }
 }
