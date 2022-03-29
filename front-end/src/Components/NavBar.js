a3dfebf46a10b4661fa825b9ef31b70f99e59890
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-03-29 21:02:24 +0300

    Recommendationsfunctionality for the search bar

--- front-end/src/Components/NavBar.js
+++ front-end/src/Components/NavBar.js
@@ -1,28 +1,23 @@
-import React from 'react'
-import MenuOption from './MenuOption'
-import SearchBar from './SearchBar'
-import image from '../Images/Logo.jpg';
+import React from "react";
+import MenuOption from "./MenuOption";
+import SearchBar from "./SearchBar";
+import logo from '../Images/Logo.jpg';
 import { v4 as uuidv4 } from "uuid";
 
 export default function NavBar() {
-  const menuOptionComponents = menuOptions.map( m => {
-    return (<MenuOption key={uuidv4()} OptionName={m}/>)
-  })
+  const menuOptionComponents = menuOptions.map((m) => {
+    return <MenuOption key={uuidv4()} OptionName={m} />;
+  });
 
   return (
-    <header className='Header'>
-        <div className='Header-left-container'>
-          <img 
-              className='Logo'
-              src={image}
-              alt='Logo'/>
-          <SearchBar />
-        </div>
-        <div className='Header-right-container'>
-          {menuOptionComponents}
-        </div>
+    <header className="Header">
+      <div className="Header-left-container">
+        <img className="Logo" src={logo} alt="Logo" />
+        <SearchBar />
+      </div>
+      <div className="Header-right-container">{menuOptionComponents}</div>
     </header>
-  )
+  );
 }
 
-const menuOptions = ['Favorites', 'My Products', 'Account'];
+const menuOptions = ["Favorites", "My Products", "Account"];
--- front-end/src/Components/SearchBar.js
+++ front-end/src/Components/SearchBar.js
@@ -1,27 +1,39 @@
 import React, { useState } from "react";
 import SearchResult from "./SearchResult";
+import searchIcon from "../Images/Search.png"
 
 export default function SearchBar() {
   const [search, setSearch] = useState();
 
   function handleSearch(value) {
     const newSearch = data.filter((d) => d.includes(value));
-    if (value === "" || newSearch.length == 0) {
+    if (value === "" || newSearch.length === 0) {
       setSearch(undefined);
     } else {
       setSearch(newSearch);
     }
   }
 
+  function showRecomandations() {
+    setSearch(data);
+  }
+
   return (
     <div>
+      <div className="Search-bar-container">
       <input
         className="SearchBar"
         type="search"
         placeholder="Type to search..."
-        onInput={(e) => handleSearch(e.target.value)}
+        onChange={(e) => handleSearch(e.target.value)}
+        onClick={(e) => showRecomandations()}
+        onBlur={(e) => setSearch(undefined)}
       ></input>
-      <span>&#128269;</span>
+      <img 
+        className="Search-Icon"
+        src={searchIcon}
+        alt="Search icon"></img>
+      </div>
       {search !== undefined ? <SearchResult data={search} /> : null}
     </div>
   );
