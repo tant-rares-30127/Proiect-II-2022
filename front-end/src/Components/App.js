30149bceeffffc8ef3af9a662be85808ef834601
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-03-30 22:20:30 +0300

    Minimal style for  the product list added

--- front-end/src/Components/App.js
+++ front-end/src/Components/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import '../Style/App.css'
 import NavBar from "./NavBar";
+import ProductList from "./ProductList";
 
 function App() {
   return (
     <>
       <NavBar />
+      <ProductList products={products}/>
     </>
   )
 }
 
 export default App;
+
+const products = ['Laptop', 'Tablet', 'Phone']
---
+++ front-end/src/Components/ProductList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { v4 as uuidv4 } from "uuid";
+
+export default function ProductList({products}) {
+const productsToDisplay = products.map( p => {
+    return (
+        <li key={uuidv4()}>
+            <a href='../../public/index.html'>{p}</a>
+        </li>
+    )
+}) 
+
+  return (
+    <div className='Product-list-container'>
+        <h2>Products</h2>
+        <ul>
+            {productsToDisplay}
+        </ul>
+    </div>
+  )
+}
