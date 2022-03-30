b2d77e34ab5462f03493f82b546d7351d0e10d96
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-03-30 23:37:28 +0300

    Image and icons for products basic

--- front-end/src/Components/BodyApp/Body.js
+++ front-end/src/Components/BodyApp/Body.js
@@ -1,11 +1,16 @@
-import React from 'react'
-import ProductList from './ProductList'
+import React from "react";
+import ProductList from "./ProductList";
+import promoImage from "../../Images/Happy_customer_online_shopping.png";
 
-export default function Body( {products} ) {
+export default function Body({ products }) {
   return (
     <div>
-        <ProductList products={products}/>
+      <div className="Main-body-container">
+        <ProductList products={products} />
+        <img
+            className="Promo-Image" 
+            src={promoImage} alt="Promo"></img>
+      </div>
     </div>
-  )
+  );
 }
-
--- front-end/src/Components/BodyApp/ProductList.js
+++ front-end/src/Components/BodyApp/ProductList.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { v4 as uuidv4 } from "uuid";
+import icon from '../../Images/Laptop.png'
 
 export default function ProductList( {products} ) {
 const productsToDisplay = products.map( p => {
     return (
         <li key={uuidv4()}>
+            <img 
+                className='Product-Icon'
+                src={icon} alt="Icon"></img>
             <a href='../../public/index.html'>{p}</a>
         </li>
     )
