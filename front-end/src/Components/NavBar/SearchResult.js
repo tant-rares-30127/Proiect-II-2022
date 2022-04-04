11cfa9c38c5907a257d4e7a43bd0a5270065d801
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-04-04 21:35:07 +0300

    Click outside search result handle added

--- front-end/src/Components/NavBar/SearchResult.js
+++ front-end/src/Components/NavBar/SearchResult.js
@@ -1,24 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-export default function SearchResult({ data, handleHideResults}) {
+export default function SearchResult({ data, handleHideResults }) {
   const dataToDisplay = data.map((d) => {
     return (
-      <a
-        key={uuidv4()}
-        href="../../dummy1"
-        rel="noreferrer"
-      >
+      <a key={uuidv4()} href="../../dummy1" rel="noreferrer">
         {d}
       </a>
     );
   });
-  return (
-    <div
-      className="SearchResult-container"
-      onBlur={(e) => handleHideResults()}
-    >
-      {dataToDisplay}
-    </div>
-  );
+
+  useEffect(() => {
+    document.body.addEventListener("click", handleHideResults);
+
+    return () =>
+      document.body.removeEventListener("click", handleHideResults);
+  });
+
+  return <div className="SearchResult-container">{dataToDisplay}</div>;
 }
