bd83a2673eeef76626f7b166698e93fdb68e9ac2
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-04-05 18:09:14 +0300

    Added icons to menu options

--- front-end/src/Components/NavBar/NavBar.js
+++ front-end/src/Components/NavBar/NavBar.js
@@ -1,20 +1,23 @@
 import React from "react";
 import MenuOption from "./MenuOption";
 import SearchBar from "./SearchBar";
-import logo from '../../Images/Logo.jpg'
+import logo from "../../Images/Logo.jpg";
 import { v4 as uuidv4 } from "uuid";
+import favoritesIcon from "../../Images/Heart.png";
+import productsIcon from "../../Images/Shop.png";
+import accountIcon from "../../Images/UserCircle.png";
 
 export default function NavBar() {
-  const menuOptionComponents = menuOptions.map((m) => {
-    return <MenuOption key={uuidv4()} OptionName={m} />;
+  const menuOptionComponents = menuOptions.map((menuOptions) => {
+    return <MenuOption key={uuidv4()} {...menuOptions} />;
   });
+
+  console.log(menuOptions)
 
   return (
     <header className="Header">
       <div className="Header-left-container">
-        <a
-          className="Image-link-container" 
-          href="../../">
+        <a className="Image-link-container" href="../../">
           <img className="Logo" src={logo} alt="Logo" />
         </a>
         <SearchBar />
@@ -24,4 +27,17 @@ export default function NavBar() {
   );
 }
 
-const menuOptions = ["Favorites", "My Products", "Account"];
+const menuOptions = [
+  {
+    name: "Favorites",
+    icon: favoritesIcon,
+  },
+  {
+    name: "My Products",
+    icon: productsIcon,
+  },
+  {
+    name: "Account",
+    icon: accountIcon,
+  },
+];
