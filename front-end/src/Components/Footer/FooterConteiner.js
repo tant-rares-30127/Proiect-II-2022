7831a0c2b18eecd5f3f074fce552ea5e0538a8f9
buliga-marioara-loredana-30127 <79511366+buliga-marioara-loredana-30127@users.noreply.github.com>
2022-04-07 21:28:58 +0300

    Icons_on_footer

--- front-end/src/Components/Footer/FooterConteiner.js
+++ front-end/src/Components/Footer/FooterConteiner.js
@@ -1,6 +1,9 @@
 import { toBeRequired } from '@testing-library/jest-dom/dist/matchers';
 import React from 'react';
 import Footer from './FooterIndex';
+import logo from '../../Images/Logo.jpg'
+import logo1 from '../../Images/Social_Frame.png'
+import logo2 from '../../Images/YT_Frame.png'
 
 export default function FooterContainer(){
     return(
@@ -31,9 +34,17 @@ export default function FooterContainer(){
                     <Footer.Link href="#">Terms and conditions</Footer.Link>
                     <Footer.Link href="#">Processing of personal data</Footer.Link>
                 </Footer.Column>
-                       
                 <Footer.Column>
-                    
+                    <a href="../../public/index.html">
+                        <img className="Logo" src={logo} alt="Logo" />
+                    </a>
+                    <a href="#">
+                        <img className="Social_Frame" src={logo1} alt="Social_Frame" />
+                    </a>
+                    <a href="#">
+                        <img className="YT_Frame" src={logo2} alt="YT_Frame" />
+                    </a>
+                    <Footer.Link href="#">IshopComunity@yahoo.com</Footer.Link>
                 </Footer.Column>
             </Footer.Row>
           
