95bee6f3c5d632535fc4ecef874e99bdc52c62dc
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-04-09 17:09:40 +0300

    First details for the promo image

--- front-end/src/Components/Register/PromoImage.js
+++ front-end/src/Components/Register/PromoImage.js
@@ -1,7 +1,11 @@
 import React from 'react'
+import promoImage from '../../Images/Get_in_touch.png'
 
 export default function PromoImage() {
   return (
-    <div>PromoImage</div>
+    <div className='PromoImage-container'>
+      <h2>Acces Is Closed. Community Members Only</h2>
+      <img className='Register-promo-image' src={promoImage} alt='Image'/>
+    </div>
   )
 }
