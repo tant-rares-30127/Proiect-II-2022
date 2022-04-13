1f46796a0c6488ceaf5f27c7301edfeed7a942f9
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-04-13 17:27:02 +0300

    Added Logo in the up right corner

--- front-end/src/Components/Register/PromoImage.js
+++ front-end/src/Components/Register/PromoImage.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import promoImage from '../../Images/Get_in_touch.png'
+import logo from '../../Images/Logo_transparent.png'
 
 export default function PromoImage() {
   return (
     <div className='PromoImage-container'>
       <h2>Acces Is Closed. Community Members Only</h2>
-      <img className='Register-promo-image' src={promoImage} alt='Promo'/>
+        <div className='PromoImage-aux-container'>
+          <img className='Register-promo-image' src={promoImage} alt='Promo'/>
+          <div className='PromoImage-elements-container'>
+            <img className='Logo Promo-Logo' src={logo} alt="Logo"/>
+          </div>
+      </div>
     </div>
   )
 }
