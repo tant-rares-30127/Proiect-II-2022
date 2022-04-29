6206b1e2f7a9b86dca2e83594052f499e290d73f
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-04-29 17:24:23 +0300

    await

--- front-end/src/Components/BodyApp/ProductList.js
+++ front-end/src/Components/BodyApp/ProductList.js
@@ -19,23 +19,79 @@ export default function ProductList() {
       })
       .then((data) => {
         newProducts = data;
-
-        const productsToDisplay = newProducts.map((p) => {
-          return (
-            <li key={uuidv4()}>
-              <a href="../../dummy2">
-                <img
-                  className="Product-Icon"
-                  src={productIcon}
-                  alt="Icon"
-                ></img>
-                <span>{p.name}</span>
-              </a>
-            </li>
-          );
+        let productsToDisplayNew = [];
+        let i = 0;
+        newProducts.map((p) => {
+          const iconsNames = p.imageAdress.split("/")[3];
+          //console.log(iconsNames);
+          let func = (async (iconsNames) => {
+            let ceva = await import(`../../Images/${iconsNames}`)
+            return ceva.default;
+          })
+          console.log(func(iconsNames))
+          // import(`../../Images/${iconsNames}`)
+          //   .then((img) => {
+          //     console.log("OK");
+          //     console.log(i)
+          //     productsToDisplayNew[i] = (
+          //       <li key={uuidv4()}>
+          //         <a href="../../dummy2">
+          //           <img
+          //             className="Product-Icon"
+          //             src={img.default}
+          //             alt="Icon"
+          //           ></img>
+          //           <span>{p.name}</span>
+          //         </a>
+          //       </li>
+          //     );
+          //     setProducts(productsToDisplayNew)
+          //     i = i + 1
+          //   })
+          //   .catch((error) => {
+          //     console.log(error);
+          //     productsToDisplayNew[i] = (
+          //       <li key={uuidv4()}>
+          //         <a href="../../dummy2">
+          //           <img
+          //             className="Product-Icon"
+          //             src={productIcon}
+          //             alt="Icon"
+          //           ></img>
+          //           <span>{p.name}</span>
+          //         </a>
+          //       </li>
+          //     );
+          //     console.log(productsToDisplayNew[i])
+          //     setProducts(productsToDisplayNew)
+          //     i = i + 1
+          //   });
         });
 
-        setProducts(productsToDisplay);
+        let ceva = "Laptop.png";
+        import(`../../Images/${ceva}`)
+          .then((img) => {
+            //console.log("OK");
+            const productsToDisplay = newProducts.map((p) => {
+              //console.log(img.default);
+              //console.log(productIcon);
+              return (
+                <li key={uuidv4()}>
+                  <a href="../../dummy2">
+                    <img
+                      className="Product-Icon"
+                      src={img.default}
+                      alt="Icon"
+                    ></img>
+                    <span>{p.name}</span>
+                  </a>
+                </li>
+              );
+            });
+            console.log(productsToDisplayNew[i])
+            //setProducts(productsToDisplay);
+          })
+          .catch((error) => console.log(error));
       });
   }, []);
 
---
+++ front-end/test.js
@@ -0,0 +1,7 @@
+import('./src/Images/Appliances.png')
+.then(() => {
+    console.log("ok");
+})
+.catch(() => {
+    console.log("not ok")
+})
