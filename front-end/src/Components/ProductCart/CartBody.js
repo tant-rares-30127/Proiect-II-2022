89a66c3d2e7693a6a7480a887d2aa3be22c3d5d4
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-05-02 20:41:57 +0300

    Cart body header implemented

--- front-end/src/Components/ProductCart/CartBody.js
+++ front-end/src/Components/ProductCart/CartBody.js
@@ -1,8 +1,39 @@
-import React from 'react'
-import CartProduct from './CartProduct'
+import React from "react";
+import CartProduct from "./CartProduct";
 
 export default function CartBody() {
   return (
-    <div>CartBody</div>
-  )
+    <div>
+      <div className="Cart-details-bar">
+        <div className="Bar-item">Product</div>
+        <div className="Amount-price-container">
+          <div className="Bar-item">Amount</div>
+          <div className="Bar-item">Price</div>
+        </div>
+      </div>
+
+      
+    </div>
+  );
 }
+
+const productsData = [
+  {
+    name: "Telefon mobul Apple",
+    description: "iPhone 11, 64 GB, Black",
+    price: 2.689,
+    amount: 1,
+  },
+  {
+    name: "Telefon mobul Apple",
+    description: "iPhone 11, 64 GB, Black",
+    price: 2.689,
+    amount: 1,
+  },
+  {
+    name: "Telefon mobul Apple",
+    description: "iPhone 11, 64 GB, Black",
+    price: 2.689,
+    amount: 1,
+  },
+];
