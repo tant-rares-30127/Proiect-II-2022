ac5ae2781de4bd12e868f3b9e3735752109043f4
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-05-02 21:17:25 +0300

    Product cart styling each product element

--- front-end/src/Components/ProductCart/CartProduct.js
+++ front-end/src/Components/ProductCart/CartProduct.js
@@ -3,22 +3,27 @@ import React from "react";
 export default function CartProduct({ data }) {
   console.log(data);
   return (
-    <div>
-      <div>
+    <div className="ProductCart-container">
+      <div className="ProductCart-image-details-container">
         <img src={data.image} alt="prod-img"></img>
-        <div>
-          <div>{data.name}</div>
+        <div className="ProductCart-details-container">
+          <div className="ProductCart-title">{data.name}</div>
           <div>{data.description}</div>
-          <div>{data.price}</div>
+          <div className="ProductCart-price">{data.price} Lei</div>
         </div>
       </div>
       <div>
-        <button>-</button>
-        <div>{data.amount}</div>
-        <button>+</button>
-      </div>
-      <div>
-        <div>{data.price}</div>
+        <div>
+          <div>
+            <button>-</button>
+            <div>{data.amount}</div>
+            <button>+</button>
+          </div>
+          <button>Remove</button>
+        </div>
+        <div>
+          <div>{data.price}</div>
+        </div>
       </div>
     </div>
   );
