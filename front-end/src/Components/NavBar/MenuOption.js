fc005b751fa11765a6443dd464fca21c36e65da5
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-05-10 20:02:42 +0300

    Nav bar but now dimensions are better

--- front-end/src/Components/NavBar/MenuOption.js
+++ front-end/src/Components/NavBar/MenuOption.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import arrowDown from '../../Images/Arrow_down.png'
+import React from "react";
+import arrowDown from "../../Images/Arrow_down.png";
 
-export default function MenuOption( {name, icon}) {
-
+export default function MenuOption({ name, icon }) {
   return (
-    <div className='MenuOption-container'>
-      <button className='MenuOption-btn'>
+    <div className="MenuOption-container">
+      <button className="MenuOption-btn">
         <div>
-          <img className='NavBar-Icon' src={icon} alt="icon"/>
+          <img className="NavBar-Icon" src={icon} alt="icon" />
         </div>
+        <div>{name}</div>
         <div>
-          {name}
-        </div>
-        <div>
-          <img src={arrowDown} alt="arrow"/>
+          <img className="Arrow-Icon" src={arrowDown} alt="arrow" />
         </div>
       </button>
-      <div className='Options-container'>
-        <a href='../../'>Link1</a>
-        <a href='../../'>Link2</a>
-        <a href='../../'>Link3</a>
+      <div>
+        <div className="Options-container">
+          <a href="../../">Link1</a>
+          <a href="../../">Link2</a>
+          <a href="../../">Link3</a>
+        </div>
       </div>
     </div>
-  )
+  );
 }
