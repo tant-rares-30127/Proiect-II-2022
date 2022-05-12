3e3e8e935fc70060ff12edb8c4203201bd4f1cb1
DESKTOP-0KQ1UDC\tantr <tantrares@yahoo.com>
2022-05-12 23:55:55 +0300

    Proper shoppingcart and shoppingcartproduct implementation

--- Proiect_II/Models/ShoppingCartProduct.cs
+++ Proiect_II/Models/ShoppingCartProduct.cs
@@ -8,6 +8,8 @@ namespace Proiect_II.Models
     public class ShoppingCartProduct
     {
         public int? Id { get; set; }
+
+        public int? ShoppingCartId { get; set; }
         public ShoppingCart ShoppingCart { get; set; }
         public Product Product { get; set; }
         public int Quantity { get; set; }
