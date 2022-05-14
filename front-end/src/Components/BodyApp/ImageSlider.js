29246243c1b8bbf90119ac932ff6d1bd0c06d2fb
iustinmarkis23@gmail.com <iustinmarkis23@gmail.com>
2022-05-14 14:39:21 +0300

    We got the AUTO SLIDER Yeah boyyy

--- front-end/src/Components/BodyApp/ImageSlider.js
+++ front-end/src/Components/BodyApp/ImageSlider.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import SliderButton from "./SliderButton";
 import { v4 as uuidv4 } from "uuid";
 import promo2Image from "../../Images/promo1.jpg";
@@ -8,29 +8,63 @@ import promo3Image from "../../Images/promo4.jpg";
 
 export default function ImageSlider() {
   const [imageToShow, setImageToShow] = useState(images[0].image);
+  const [power, setPower] = useState(false)
+  const [auto, setAuto] = useState(false)
 
-  const buttonsContainer =  (
-      <div className="SliderButtons-container">
-        {images.map((image) => {
-          let isChecked = false
-          if (image.image === imageToShow) {
-            isChecked = true
-          }
-          return (
-            <SliderButton
-              key={uuidv4()}
-              handleClick={handleSliderButtonClick}
-              imageKey={image.key}
-              isChecked={isChecked}
-            />
-          );
-        })}
-      </div>
-    )
+  useEffect(() => {
+    setTimeout(powerAutoslider, 6000)
+    setTimeout(() => setAuto(true), 1000)
+  }, [])
+
+  useEffect(() => {
+    autoSlider()
+
+  }, [power])
+
+  const buttonsContainer = (
+    <div className="SliderButtons-container">
+      {images.map((image) => {
+        let isChecked = false;
+        if (image.image === imageToShow) {
+          isChecked = true;
+        }
+        return (
+          <SliderButton
+            key={uuidv4()}
+            handleClick={handleSliderButtonClick}
+            imageKey={image.key}
+            isChecked={isChecked}
+          />
+        );
+      })}
+    </div>
+  );
+
+  async function powerAutoslider() {
+    setPower(prev => !prev)
+    if (auto === false) {
+      setAuto(prev => !prev)
+    }
+    setTimeout(powerAutoslider, 6000)
+  }
+
+  function autoSlider() {
+    if (auto === true) {
+      let newImage = images.find(i => i.image === imageToShow)
+      let index = images.indexOf(newImage)
+      if ((index + 1) > (images.length - 1)) {
+        newImage = images[0]
+      } else {
+        newImage = images[index + 1]
+      }
+      setImageToShow(newImage.image)
+    } 
+  }
 
   function handleSliderButtonClick(key) {
     let newImage = images.filter((element) => element.key === key);
     setImageToShow(newImage[0].image);
+    setAuto(false)
   }
 
   return (
@@ -58,6 +92,6 @@ const images = [
   },
   {
     image: promo3Image,
-    key: uuidv4()
-  }
+    key: uuidv4(),
+  },
 ];
