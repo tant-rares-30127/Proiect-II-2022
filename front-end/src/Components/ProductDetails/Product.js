339cfdf824805d116e94837e57bbfb566b2aa20b
buliga-marioara-loredana-30127 <79511366+buliga-marioara-loredana-30127@users.noreply.github.com>
2022-05-21 00:35:35 +0300

    display the individual product from listProduct

--- front-end/src/Components/ProductDetails/Product.js
+++ front-end/src/Components/ProductDetails/Product.js
@@ -1,27 +1,30 @@
 import React from "react";
 import productImage from "../../Images/Someting_new.png";
 import heartIcon from "../../Images/Heart.png";
-import starIcon from "../../Images/fullStar.png"
+import starIcon from "../../Images/fullStar.png";
 
 export default function Product({ product }) {
   return (
     <div className="Main-details-container">
       <div className="Product-details-container">
         <div className="Product-image-container">
-          <img src={product === undefined ? null : product.imageAdress} alt="Product" />
+          <img
+            src={product === undefined ? null : product.imageAdress}
+            alt="Product"
+          />
         </div>
         <div className="Product-elemnents-container">
           <div className="Product-name-container">
             <h2>{product === undefined ? null : product.name}</h2>
-            <div>4.6 <img className="NavBar-Icon" src={starIcon} alt="starIcon" /></div>
+            <div>
+              4.6 <img className="NavBar-Icon" src={starIcon} alt="starIcon" />
+            </div>
             <div className="Price-detail-container">
               <div>{product === undefined ? null : product.price}</div>
               <div>Lei</div>
             </div>
           </div>
-          <p>
-            &emsp;{product === undefined ? null : product.description}
-          </p>
+          <p>&emsp;{product === undefined ? null : product.description}</p>
           <div className="Details-buttons-container">
             <button className="Buy-product-btn">Buy now</button>
             <button className="Fav-product-btn">
--- front-end/src/Components/ProductDetails/ProductDetails.js
+++ front-end/src/Components/ProductDetails/ProductDetails.js
@@ -4,28 +4,36 @@ import Product from "./Product";
 import DetailsTab from "./DetailsTab";
 
 export default function ProductDetails() {
-  const [product, setProduct] = useState()
+  const [product, setProduct] = useState();
 
   useEffect(() => {
-    axios.get("https://localhost:5001/Products/ReturnProduct?productName=Laptop MacBook")
-    .then((response) => response.data)
-    .then((data) => {
-      console.log(data)
-      setProduct(data)
-    })
-    .catch((error) => console.log(error))
-    .catch((error) => console.log(error))
-  }, [])
+    let product_name = localStorage.getItem("productName");
+    console.log(product_name);
+    axios
+      .get(
+        "https://localhost:5001/Products/ReturnProduct?productName=" +
+          product_name
+      )
+      .then((response) => response.data)
+      .then((data) => {
+        console.log(data);
+        setProduct(data);
+      })
+      .catch((error) => console.log(error))
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <div className="Product-details-body-container">
       <div className="Product-details-title-container">
         <h2>Product Details</h2>
       </div>
-      <Product product={product}/>
-      <DetailsTab description={product === undefined ? "No description" : product.description}/>
+      <Product product={product} />
+      <DetailsTab
+        description={
+          product === undefined ? "No description" : product.description
+        }
+      />
     </div>
   );
 }
-
-
