e405ebe316f226fe3f076bc11a6ea87e7119f5f0
DESKTOP-0KQ1UDC\tantr <tantrares@yahoo.com>
2022-05-20 21:36:37 +0300

    Adding Order History Model

---
+++ Proiect_II/Models/OrderHistory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proiect_II.Models
+{
+    public class OrderHistory
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string Address { get; set; }
+        public string PostalCode { get; set; }
+        public string PaymentMethod { get; set; }
+        public DateTime OrderDate { get; set; }
+        public ShoppingCart ShoppingCart { get; set; }
+    }
+}
