993c02be6b0453a5805a744ce52f0c297cd8c156
buliga-marioara-loredana-30127 <79511366+buliga-marioara-loredana-30127@users.noreply.github.com>
2022-05-20 23:58:24 +0300

    display the productsList for productType

--- front-end/src/Components/Products_Page/Phone_List.js
+++ front-end/src/Components/Products_Page/Phone_List.js
@@ -3,36 +3,41 @@ import Phones_products from "./Phones_Products";
 import "./Styles/Products.css";
 import { useNavigate } from "react-router-dom";
 
-export default function Phone_List() {
+export default function Phone_List({ selectedProducts }) {
   const navigate = useNavigate();
-
+  console.log(selectedProducts);
   const handleProduct = (product_name) => {
     localStorage.setItem("productName", product_name);
     let path = "/productDetails";
     navigate(path);
   };
 
-  let productData = Phones_products.map((item) => (
-    <div onClick={() => handleProduct(item.product_name)} className="Card">
-      <div className="card_img">
-        <img id="object-position" src={item.thumb}></img>
-      </div>
-      <div className=" Card_body">
-        <h2>{item.product_name}</h2>
-        <p>{item.description}</p>
-        <div className="Price_section">
-          <div className="Price">
-            <h1>{item.price} lei</h1>
+  let productData = null;
+
+  if (selectedProducts !== undefined) {
+    productData = selectedProducts.map((item) => (
+      <div onClick={() => handleProduct(item.name)} className="Card">
+        <div className="card_img">
+          <img id="object-position" src={item.imageAdress}></img>
+        </div>
+        <div className=" Card_body">
+          <h2>{item.name}</h2>
+          <p>{item.description}</p>
+          <div className="Price_section">
+            <div className="Price">
+              <h1>{item.price} lei</h1>
+            </div>
+            <div>
+              <h3>{item.price} lei</h3>
+            </div>
           </div>
-          <div>
-            <h3>{item.abonament_price} lei</h3>
+          <div className="Button">
+            <div>Buy now</div>
           </div>
         </div>
-        <div className="Button">
-          <div>Buy now</div>
-        </div>
       </div>
-    </div>
-  ));
+    ));
+  }
+
   return <>{productData}</>;
 }
--- front-end/src/Components/Products_Page/ProductsPage.js
+++ front-end/src/Components/Products_Page/ProductsPage.js
@@ -1,10 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Dropdown from "./Dropdown";
 import Products from "./Products";
 import "./Styles/ProductsPage.css";
+import axios from "axios";
 
 export default function ProductsPage() {
   const [selected, setIsSelected] = useState("");
+
+  const [selectedProducts, setIsSelectedProducts] = useState();
+
+  useEffect(() => {
+    let productType = localStorage.getItem("productType");
+    console.log(productType);
+    axios
+      .get(
+        "https://localhost:5001/ProductTypes/ProductsForProductType?text=" +
+          productType
+      )
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .then((data) => {
+        console.log(data);
+        setIsSelectedProducts(data);
+      });
+  }, []);
+
   return (
     <div className="products_container">
       <div className="dropdown_filters">
@@ -17,7 +41,7 @@ export default function ProductsPage() {
           <div className="name">Phones</div>
           <div className="results">(134 results)</div>
         </div>
-        <Products></Products>
+        <Products selectedProducts={selectedProducts}></Products>
       </div>
     </div>
   );
