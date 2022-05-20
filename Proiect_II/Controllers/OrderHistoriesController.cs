98c70bcdbe0b3095d8e1541bb60ba7f33de40559
DESKTOP-0KQ1UDC\tantr <tantrares@yahoo.com>
2022-05-21 00:00:55 +0300

    Adding Order history

---
+++ Proiect_II/Controllers/OrderHistoriesController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Proiect_II.Data;
+using Proiect_II.Models;
+
+namespace Proiect_II.Controllers
+{
+    public class OrderHistoriesController : Controller
+    {
+        private readonly Proiect_IIContext _context;
+
+        public OrderHistoriesController(Proiect_IIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: OrderHistories
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.OrderHistory.ToListAsync());
+        }
+
+        // GET: OrderHistories/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderHistory = await _context.OrderHistory
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (orderHistory == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderHistory);
+        }
+
+        // GET: OrderHistories/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OrderHistories/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email,Phone,City,County,Address,PostalCode,PaymentMethod,OrderDate")] OrderHistory orderHistory)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(orderHistory);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(orderHistory);
+        }
+
+        // GET: OrderHistories/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderHistory = await _context.OrderHistory.FindAsync(id);
+            if (orderHistory == null)
+            {
+                return NotFound();
+            }
+            return View(orderHistory);
+        }
+
+        // POST: OrderHistories/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,Phone,City,County,Address,PostalCode,PaymentMethod,OrderDate")] OrderHistory orderHistory)
+        {
+            if (id != orderHistory.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(orderHistory);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderHistoryExists(orderHistory.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(orderHistory);
+        }
+
+        // GET: OrderHistories/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var orderHistory = await _context.OrderHistory
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (orderHistory == null)
+            {
+                return NotFound();
+            }
+
+            return View(orderHistory);
+        }
+
+        // POST: OrderHistories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var orderHistory = await _context.OrderHistory.FindAsync(id);
+            _context.OrderHistory.Remove(orderHistory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool OrderHistoryExists(int id)
+        {
+            return _context.OrderHistory.Any(e => e.Id == id);
+        }
+    }
+}
