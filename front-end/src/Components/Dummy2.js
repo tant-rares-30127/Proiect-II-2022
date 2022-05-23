b6368c9d3888f0e874158a0c5ee521b9ca1e6b66
DESKTOP-0KQ1UDC\tantr <tantrares@yahoo.com>
2022-05-24 00:11:26 +0300

    Revert "Dummy change"
    This reverts commit 3a6c656ff3e0c76bfdb34b530b776cfc07c5409d.

--- front-end/src/Components/Dummy2.js
+++ front-end/src/Components/Dummy2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import CheckoutPage from "./CheckOut/CheckoutPage";
-//import ClientType from "./CheckOut/ClientType";
+import ClientType from "./CheckOut/ClientType";
 import NameForm from "./CheckOut/NameForm";
 
 export default function Dummy2() {
